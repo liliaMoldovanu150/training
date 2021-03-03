3561cc5d56f4f2e5c4dbb44433a53e85479e3288
Lilia Moldovanu <lilia.moldovanu@5psolutions.eu>
2021-03-03 14:06:09 +0200

    {4} {index.php} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-lilia-moldovanu/tasks/4} This commit will fix the code to make it

--- common.php
+++ common.php
@@ -1,12 +1,15 @@
 <?php
 
+session_start();
+
 require_once './config.php';
 
 try {
 
-    $pdo = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname, $username, $password);
+    $GLOBALS['pdo'] = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname, $username, $password);
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $GLOBALS['pdo']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Could not connect to database.' . $e->getMessage();
 }
+
--- index.php
+++ index.php
@@ -1,33 +1,30 @@
 <?php
 
-session_start();
-
 require_once 'common.php';
 
 if (!isset($_SESSION['id'])) {
     $_SESSION['id'] = array();
 }
 
-if(!empty($_GET)) {
-    array_push($_SESSION['id'],$_GET['id']);
-    header("Location: index.php");
+if (!empty($_GET['id'])) {
+    array_push($_SESSION['id'], $_GET['id']);
+    header('Location: index.php');
 }
 
 try {
     $cartIds = $_SESSION['id'];
     $inQuery = implode(',', array_fill(0, count($cartIds), '?'));
-    $condition = empty($_SESSION['id']) ? '' : ' WHERE id NOT IN(' . $inQuery . ')';
+    $condition = empty($_SESSION['id']) ? '' : ' WHERE id NOT IN(' . $inQuery . ');';
     $sql = 'SELECT * FROM products' . $condition;
-    $stmt = $pdo->prepare($sql);
-    foreach ($cartIds as $k => $id)
+    $stmt = $GLOBALS['pdo']->prepare($sql);
+    foreach ($cartIds as $k => $id) {
         $stmt->bindValue(($k+1), $id);
+    }
     $stmt->execute();
-
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-} catch(PDOException $e) {
-    'Could not fetch products.' . $e->getMessage();
+} catch (PDOException $e) {
+    echo 'Could not fetch products.' . $e->getMessage();
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -39,24 +36,24 @@ try {
     </head>
     <body>
         <div class="container">
-            <?php if(!empty($products)) {
-                foreach($products as $product) : ?>
+            <?php if (!empty($products)): ?>
+                <?php foreach ($products as $product): ?>
                     <div class="product-item">
                         <div class="product-image">
-                            <img src="<?php echo $product['image_url']; ?>" alt="product-image">
+                            <img src="<?= $product['image_url']; ?>" alt="product-image">
                         </div>
                         <div class="product-features">
-                            <div><?= $product['title'];?></div>
-                            <div><?= $product['description'];?></div>
-                            <div><?= $product['price'];?></div>
+                            <div><?= $product['title']; ?></div>
+                            <div><?= $product['description']; ?></div>
+                            <div><?= $product['price']; ?></div>
                         </div>
-                        <a href="index.php?id=<?php echo $product['id'];?>">Add</a>
+                        <a href="index.php?id=<?= $product['id']; ?>">Add</a>
                     </div>
-                <br>
-                <?php endforeach;
-            } else { ?>
+                    <br>
+                <?php endforeach; ?>
+            <?php else: ?>
                 <p>All products were added to cart.</p>
-            <?php } ?>
+            <?php endif; ?>
             <a class="to-cart" href="cart.php">Go to cart</a>
         </div>
     </body>
