e8c5e71b9e50798a21cc71843f955e63e8f01df8
Lilia Moldovanu <lilia.moldovanu@5psolutions.eu>
2021-03-12 13:22:26 +0200

    {#4} {index.php} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-lilia-moldovanu/tasks/4} This commit will fix all bugs from review

--- common.php
+++ common.php
@@ -21,27 +21,19 @@ function connection()
 
 function getAllProducts(): array
 {
-    try {
-        $sql = 'SELECT * FROM products';
-        $stmt = connection()->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        throw $e;
-    }
+    $sql = 'SELECT * FROM products';
+    $stmt = connection()->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function productExists($productId): array
 {
-    try {
-        $sql = 'SELECT * FROM products WHERE id=?;';
-        $stmt = connection()->prepare($sql);
-        $stmt->bindParam(1, $productId, PDO::PARAM_INT);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        throw $e;
-    }
+    $sql = 'SELECT * FROM products WHERE id=?;';
+    $stmt = connection()->prepare($sql);
+    $stmt->bindParam(1, $productId, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function getCartProducts($cartIds): array
@@ -50,17 +42,12 @@ function getCartProducts($cartIds): array
         return [];
     }
 
-    try {
-        $inQuery = implode(',', array_fill(0, count($cartIds), '?'));
-        $sql = 'SELECT * FROM products WHERE id IN (' . $inQuery . ');';
-        $stmt = connection()->prepare($sql);
-        $stmt->execute($cartIds);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        throw $e;
-    }
+    $inQuery = implode(',', array_fill(0, count($cartIds), '?'));
+    $sql = 'SELECT * FROM products WHERE id IN (' . $inQuery . ');';
+    $stmt = connection()->prepare($sql);
+    $stmt->execute($cartIds);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-
 
 function removeItemFromCart(): array
 {
@@ -78,58 +65,53 @@ function translate($label): string
 
 function uploadImage()
 {
-    try {
-        if (
-            !isset($_FILES['image']['error']) ||
-            is_array($_FILES['image']['error'])
-        ) {
-            throw new RuntimeException('Invalid parameters.');
-        }
-
-        switch ($_FILES['image']['error']) {
-            case UPLOAD_ERR_OK:
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                throw new RuntimeException('No file sent.');
-            case UPLOAD_ERR_INI_SIZE:
-            case UPLOAD_ERR_FORM_SIZE:
-                throw new RuntimeException('Exceeded filesize limit.');
-            default:
-                throw new RuntimeException('Unknown errors.');
-        }
-
-        if ($_FILES['image']['size'] > 1000000) {
-            throw new RuntimeException('Exceeded filesize limit.');
-        }
-
-        $finfo = new finfo(FILEINFO_MIME_TYPE);
-
-        if (false === $ext = array_search(
-                $finfo->file($_FILES['image']['tmp_name']),
-                array(
-                    'jpg' => 'image/jpeg',
-                    'png' => 'image/png',
-                    'gif' => 'image/gif',
-                ),
-                true
-            )) {
-            throw new RuntimeException('Invalid file format.');
-        }
-
-        if (!move_uploaded_file(
-            $_FILES['image']['tmp_name'],
-            sprintf('./images/%s.%s',
-                sha1_file($_FILES['image']['tmp_name']),
-                $ext
-            )
-        )) {
-            throw new RuntimeException('Failed to move uploaded file.');
-        }
-        return true;
-
-    } catch (RuntimeException $e) {
-        throw $e;
+    if (
+        !isset($_FILES['image']['error']) ||
+        is_array($_FILES['image']['error'])
+    ) {
+        throw new RuntimeException('Invalid parameters.');
     }
+
+    switch ($_FILES['image']['error']) {
+        case UPLOAD_ERR_OK:
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            throw new RuntimeException('No file sent.');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            throw new RuntimeException('Exceeded filesize limit.');
+        default:
+            throw new RuntimeException('Unknown errors.');
+    }
+
+    if ($_FILES['image']['size'] > 1000000) {
+        throw new RuntimeException('Exceeded filesize limit.');
+    }
+
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+
+    if (false === $ext = array_search(
+            $finfo->file($_FILES['image']['tmp_name']),
+            array(
+                'jpg' => 'image/jpeg',
+                'png' => 'image/png',
+                'gif' => 'image/gif',
+            ),
+            true
+        )) {
+        throw new RuntimeException('Invalid file format.');
+    }
+
+    if (!move_uploaded_file(
+        $_FILES['image']['tmp_name'],
+        sprintf('./images/%s.%s',
+            sha1_file($_FILES['image']['tmp_name']),
+            $ext
+        )
+    )) {
+        throw new RuntimeException('Failed to move uploaded file.');
+    }
+    return true;
 }
 
 
--- index.php
+++ index.php
@@ -5,16 +5,12 @@ require_once './common.php';
 if (!count($_SESSION['id'])) {
     $products = getAllProducts();
 } else {
-    try {
-        $cartIds = $_SESSION['id'];
-        $inQuery = implode(',', array_fill(0, count($cartIds), '?'));
-        $sql = 'SELECT * FROM products WHERE id NOT IN (' . $inQuery . ');';
-        $stmt = connection()->prepare($sql);
-        $stmt->execute($cartIds);
-        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        throw $e;
-    }
+    $cartIds = $_SESSION['id'];
+    $inQuery = implode(',', array_fill(0, count($cartIds), '?'));
+    $sql = 'SELECT * FROM products WHERE id NOT IN (' . $inQuery . ');';
+    $stmt = connection()->prepare($sql);
+    $stmt->execute($cartIds);
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -29,8 +25,8 @@ if (!count($_SESSION['id'])) {
                     <img src="./images/<?= $product['image_url']; ?>" alt="<?= translate('product_image'); ?>">
                 </div>
                 <div class="product-features">
-                    <div><?= ucfirst($product['title']); ?></div>
-                    <div><?= ucfirst($product['description']); ?></div>
+                    <div><?= $product['title']; ?></div>
+                    <div><?= $product['description']; ?></div>
                     <div><?= $product['price']; ?></div>
                 </div>
                 <form action="./cart.php" method="post">
@@ -42,7 +38,7 @@ if (!count($_SESSION['id'])) {
             <br>
         <?php endforeach; ?>
     <?php else: ?>
-        <p class="message">All products were added to cart.</p>
+        <p class="message"><?= translate('all_added'); ?></p>
     <?php endif; ?>
     <a class="go" href="./cart.php"><?= translate('go_to_cart'); ?></a>
 </div>
